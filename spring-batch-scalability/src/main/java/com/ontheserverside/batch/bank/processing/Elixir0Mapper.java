9daca20a92ecf8f6efb7f2ea9b20d5b664cd4acc
Piotr Dyraga <piotr.dyraga@softwaremill.com>
2014-02-18 16:51:06 +0100

    Elixir0 import job - introduced step loading E0 message into data storage, spring batch and hibernate infrastructure configured

---
+++ spring-batch-scalability/src/main/java/com/ontheserverside/batch/bank/processing/Elixir0Mapper.java
@@ -0,0 +1,45 @@
+package com.ontheserverside.batch.bank.processing;
+
+import com.ontheserverside.batch.bank.tx.Elixir0Transaction;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public final class Elixir0Mapper implements FieldSetMapper<Elixir0Transaction> {
+
+    /**
+     * Elixir0 message column names definition as used by {@link org.springframework.batch.item.file.transform.LineTokenizer}.
+     */
+    public static final String COLUMN_NAMES = "paymentCode,paymentDate,amount,orderingPartySortCode,unused1,orderingPartyAccountNumber,"
+                    + "beneficiaryAccountNumber,orderingPartyNameAndAddress,beneficiaryNameAndAddress,unused2,beneficiarySortCode,"
+                    + "paymentDetails,unused3,unused4,transactionCode,clientBankInformation";
+
+
+    @Override
+    public Elixir0Transaction mapFieldSet(final FieldSet fieldSet) throws BindException {
+
+        final Elixir0Transaction tx = new Elixir0Transaction();
+
+        tx.setPaymentCode(fieldSet.readInt("paymentCode"));
+        tx.setPaymentDate(fieldSet.readDate("paymentDate", "yyyyMMdd"));
+        tx.setAmount(fieldSet.readBigDecimal("amount"));
+        tx.setOrderingPartySortCode(fieldSet.readString("orderingPartySortCode"));
+        tx.setOrderingPartyAccountNumber(fieldSet.readString("orderingPartyAccountNumber"));
+        tx.setBeneficiaryAccountNumber(fieldSet.readString("beneficiaryAccountNumber"));
+
+        final String[] orderingPartyNameAndAddress = fieldSet.readString("orderingPartyNameAndAddress").split("\\|", 2);
+        tx.setOrderingPartyName(orderingPartyNameAndAddress[0]);
+        tx.setOrderingPartyAddress(orderingPartyNameAndAddress[1]);
+
+        final String[] beneficiaryNameAndAddress = fieldSet.readString("beneficiaryNameAndAddress").split("\\|", 2);
+        tx.setBeneficiaryName(beneficiaryNameAndAddress[0]);
+        tx.setBeneficiaryAddress(beneficiaryNameAndAddress[1]);
+
+        tx.setBeneficiarySortCode(fieldSet.readString("beneficiarySortCode"));
+        tx.setPaymentDetails(fieldSet.readString("paymentDetails"));
+        tx.setTransactionCode(fieldSet.readString("transactionCode"));
+        tx.setClientBankInformation(fieldSet.readString("clientBankInformation"));
+
+        return tx;
+    }
+}
---
+++ spring-batch-scalability/src/test/java/com/ontheserverside/batch/bank/job/Elixir0LoadStepTest.java
@@ -0,0 +1,83 @@
+package com.ontheserverside.batch.bank.job;
+
+import com.ontheserverside.batch.bank.tx.Elixir0Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.*;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@ActiveProfiles("test")
+public class Elixir0LoadStepTest {
+
+    @Configuration
+    @ImportResource({ "classpath:/META-INF/spring/hibernateContext.xml", "classpath:/META-INF/spring/batchContext.xml" })
+    static class ApplicationContext {
+        @Bean
+        public JobLauncherTestUtils jobLauncherTestUtils() {
+          return new JobLauncherTestUtils();
+        }
+    }
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Test
+    public void shouldLoadElixir0TxFromFile() throws Exception {
+        final JobParameters jobParameters = new JobParametersBuilder()
+                .addString("fileName", this.getClass().getResource("/elixir0/5Tx.elixir0").getPath())
+                .toJobParameters();
+
+        final JobExecution jobExecution = jobLauncherTestUtils.launchStep("elixir0LoadStep", jobParameters);
+
+        final StepExecution stepExecution = jobExecution.getStepExecutions().iterator().next();
+        assertThat(stepExecution.getExitStatus(), is(ExitStatus.COMPLETED));
+        assertThat(stepExecution.getStatus(), is(BatchStatus.COMPLETED));
+        assertThat(stepExecution.getReadCount(), is(5)); // 5 is a number of TX in the imported file
+        assertThat(stepExecution.getWriteCount(), is(5));
+
+        final List<Elixir0Transaction> importedTransactions =
+                sessionFactory.openSession()
+                        .createCriteria(Elixir0Transaction.class)
+                        .addOrder(Order.asc("paymentDate"))
+                        .list();
+        assertThat(importedTransactions.size(), is(5));
+
+        // we'll test mapping of the representative transaction, the rest can be proved by induction ;-)
+        final Elixir0Transaction transaction = importedTransactions.get(0);
+        assertThat(transaction.getPaymentCode(), is(110));
+        assertThat(transaction.getPaymentDate().getTime(), is(new SimpleDateFormat("yyyyMMdd").parse("20140212").getTime()));
+        assertThat(transaction.getAmount().compareTo(BigDecimal.valueOf(828749700184127L)), is(0));
+        assertThat(transaction.getOrderingPartySortCode(), is("68578953"));
+        assertThat(transaction.getOrderingPartyAccountNumber(), is("87685789534046696210183491"));
+        assertThat(transaction.getBeneficiaryAccountNumber(), is("81878754069967016032059101"));
+        assertThat(transaction.getOrderingPartyName(), is("SOUTHBOUND LTD."));
+        assertThat(transaction.getOrderingPartyAddress(), is("8876 Harvest Leaf Harbour, Fighting Rock Corner, Maryland, 21069-9972, US"));
+        assertThat(transaction.getBeneficiaryName(), is("Vincenza Henkle"));
+        assertThat(transaction.getBeneficiaryAddress(), is("4489 Sunny Hickory Terrace, Enderby, Iowa, 50157-9431, US"));
+        assertThat(transaction.getBeneficiarySortCode(), is("87875406"));
+        assertThat(transaction.getPaymentDetails(), is("passkey deleaves rimmed sarsens sponsor"));
+        assertThat(transaction.getTransactionCode(), is("51"));
+        assertThat(transaction.getClientBankInformation(), is(""));
+    }
+}
