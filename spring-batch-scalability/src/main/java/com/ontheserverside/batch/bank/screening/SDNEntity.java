1b721b3ab55f52a5c830a824838302d514e4a65e
Piotr Dyraga <piotr.dyraga@softwaremill.com>
2014-02-21 20:48:02 +0100

    Introduced SDNEntity backed up by DB view This entity is intended to facilitate payment sanction screening process

---
+++ spring-batch-scalability/src/main/java/com/ontheserverside/batch/bank/screening/SDNEntity.java
@@ -0,0 +1,46 @@
+package com.ontheserverside.batch.bank.screening;
+
+import org.hibernate.annotations.Immutable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Immutable
+@Table(name = "SDN_ENTITY")
+public final class SDNEntity {
+
+    @Id
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "COUNTRY")
+    private String country;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+}
